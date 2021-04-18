28e4e4598df66b1778c48acf6acc5750c2334096
Maroon20 <marwen.ghodben@sesame.com.tn>
2021-04-18 11:47:07 +0000

    employee backend commit

---
+++ src/main/java/net/guides/springboot/crud/model/Employee.java
@@ -0,0 +1,99 @@
+package net.guides.springboot.crud.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document (collection = "Employee")
+public class Employee {
+
+	@Transient
+    public static final String SEQUENCE_NAME = "users_sequence";
+	
+	@Id
+	private long id;
+	
+	@NotBlank
+    @Size(max=100)
+    @Indexed(unique=true)
+	private String firstName;
+	private String lastName;
+	
+	@NotBlank
+    @Size(max=100)
+    @Indexed(unique=true)
+	private String emailId;
+	
+	private int age;
+	
+	private String city;
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Employee(long id, @NotBlank @Size(max = 100) String firstName, String lastName,
+			@NotBlank @Size(max = 100) String emailId, int age, String city) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.age = age;
+		this.city = city;
+	}
+
+	public Employee() {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ ", age=" + age + ", city=" + city + "]";
+	}	
+}
